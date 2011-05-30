3792851fdb81cc65eb228ca69f9e5914593bfb2f
Brice Morin <brice.morin1@gmail.com>
2011-05-30 22:13:23 +0200

    sample+test with events taking parameters

---
+++ src/main/scala/org/sintef/smac/samples/parameters/HelloWorld.scala
@@ -0,0 +1,318 @@
+/**
+ * Licensed under the GNU LESSER GENERAL PUBLIC LICENSE, Version 3, 29 June 2007;
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * 	http://www.gnu.org/licenses/lgpl-3.0.txt
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * Authors: Brice Morin
+ * Company: SINTEF IKT, Oslo, Norway
+ * Date: 2011
+ */
+package org.sintef.smac.samples.parameters
+
+import java.awt.GridBagConstraints
+import java.awt.GridBagLayout
+import java.awt.Insets
+import java.awt.event.ActionEvent
+import java.awt.event.ActionListener
+import javax.swing.JButton
+import javax.swing.JFrame
+import javax.swing.JLabel
+import javax.swing.JPanel
+import javax.swing.JTextPane
+import org.sintef.smac._
+
+case class LetterEvent(letter : String) extends Event {}									
+
+case class HelloWorld2StateMachine(master : Orchestrator, parent : CompositeState, keepHistory : Boolean, withGUI : Boolean) extends CompositeState(master, parent, keepHistory) {
+
+  //create sub-states
+  val INIT_state = INIT(master, this)
+  val H_state = H(master, this)
+  val E_state = E(master, this)
+  val L1_state = L1(master, this)
+  val L2_state = L2(master, this)
+  val O_state = O(master, this)
+  val STOP_state = STOP(master, this)
+  override val substates = List(INIT_state, H_state, E_state, L1_state, L2_state, O_state, STOP_state)
+  override val initial = INIT_state
+  
+  //create transitions among sub-states
+  val INIT_next_H_transition = INIT_Next_H(INIT_state, H_state, master, List(LetterEvent(null)))
+  val H_next_E_transition = H_Next_E(H_state, E_state, master, List(LetterEvent(null)))
+  val E_next_L1_transition = E_Next_L1(E_state, L1_state, master, List(LetterEvent(null)))
+  val L1_next_L2_transition = L1_Next_L2(L1_state, L2_state, master, List(LetterEvent(null)))
+  val L2_next_O_transition = L2_Next_O(L2_state, O_state, master, List(LetterEvent(null)))
+  val O_next_STOP_transition = O_Next_STOP(O_state, STOP_state, master, List())
+  override val outGoingTransitions = List(INIT_next_H_transition, H_next_E_transition, E_next_L1_transition, L1_next_L2_transition, L2_next_O_transition, O_next_STOP_transition)    
+  
+  override def startState() = {
+    super.startState
+    if (withGUI)
+      HelloWorldGUI.init
+  }
+  
+  override def onEntry() = {
+    println("start")
+  }
+  
+  override def onExit() = {
+    //TODO: define actions here
+  }
+  
+  //This is just an ugly GUI...
+  object HelloWorldGUI extends ActionListener {
+    val frame = new JFrame("HelloWorld Client")
+    val screen = new JTextPane()
+	
+    val sendH : JButton = new JButton("Send")
+    val sendE : JButton = new JButton("Send")
+    val sendL : JButton = new JButton("Send")
+    val sendO : JButton = new JButton("Send")
+
+    def init {
+      frame.setLayout(new GridBagLayout());
+      frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+      var c : GridBagConstraints = new GridBagConstraints();
+      c.gridwidth = 1;
+      c.fill = GridBagConstraints.HORIZONTAL;
+      c.insets = new Insets(0,3,0,3);
+    
+      //GUI related to H
+      c.gridy = 0;
+      c.gridx = 0;
+      frame.add(new JLabel("H"), c);
+    
+      c.gridy = 1;
+      c.gridx = 0;
+      frame.add(createPanel(), c);
+      
+      c.gridy = 2;
+      c.gridx = 0;
+      frame.add(sendH, c);
+      sendH.addActionListener(this)
+      
+      //GUI related to E
+      c.gridy = 0;
+      c.gridx = 1;
+      frame.add(new JLabel("E"), c);
+    
+      c.gridy = 1;
+      c.gridx = 1;
+      frame.add(createPanel(), c);
+      
+      c.gridy = 2;
+      c.gridx = 1;
+      frame.add(sendE, c);
+      sendE.addActionListener(this)
+      
+      //GUI related to L
+      c.gridy = 0;
+      c.gridx = 2;
+      frame.add(new JLabel("L"), c);
+    
+      c.gridy = 1;
+      c.gridx = 2;
+      frame.add(createPanel(), c);
+      
+      c.gridy = 2;
+      c.gridx = 2;
+      frame.add(sendL, c);
+      sendL.addActionListener(this)
+
+      //GUI related to O
+      c.gridy = 0;
+      c.gridx = 3;
+      frame.add(new JLabel("O"), c);
+    
+      c.gridy = 1;
+      c.gridx = 3;
+      frame.add(createPanel(), c);
+      
+      c.gridy = 2;
+      c.gridx = 3;
+      frame.add(sendO, c);
+      sendO.addActionListener(this)
+            
+      frame.pack
+      frame.setVisible(true)
+    }
+    
+    def createPanel() : JPanel = {
+      var c : GridBagConstraints = new GridBagConstraints();
+      c.fill = GridBagConstraints.HORIZONTAL;
+      c.weightx = 0.5;
+      new JPanel(new GridBagLayout());
+    }
+    
+    def actionPerformed(ae : ActionEvent) = {
+      ae.getSource match {
+        case b : JButton =>
+          if (b == sendH) {
+            master ! new LetterEvent("H")
+          }
+          else if (b == sendE) {
+            master ! new LetterEvent("E")
+          }
+          else if (b == sendL) {
+            master ! new LetterEvent("L")
+          }
+          else if (b == sendO) {
+            master ! new LetterEvent("O")
+          }
+      }
+    }
+    
+  }
+}
+
+case class INIT(master : Orchestrator, parent : CompositeState) extends State(master, parent) {
+  override def onEntry() = {
+    println("init")
+  }
+  
+  override def onExit() = {
+    //TODO: define actions here
+  }
+}
+
+case class INIT_Next_H(previous : State, next : State, master : Orchestrator, events : List[Event]) extends Transition(previous, next, master, events) { 
+  
+  override def checkGuard : Boolean = {
+    eventsMap.keys.filter{e => e.isInstanceOf[LetterEvent]}.headOption match {
+      case Some(t) => t.asInstanceOf[LetterEvent].letter.equals("H")
+      case None => true
+    }
+  }
+  
+  def executeActions() = {
+    //TODO: define actions here
+  }
+}
+
+case class H( master : Orchestrator,  parent : CompositeState) extends State(master, parent) {
+  override def onEntry() = {
+    println("W")
+  }
+  
+  override def onExit() = {
+    //TODO: define actions here
+  }
+}
+
+case class H_Next_E(previous : State, next : State, master : Orchestrator, events : List[Event]) extends Transition(previous, next, master, events) { 
+  override def checkGuard : Boolean = {
+    eventsMap.keys.filter{e => e.isInstanceOf[LetterEvent]}.headOption match {
+      case Some(t) => t.asInstanceOf[LetterEvent].letter.equals("E")
+      case None => true
+    }
+  }
+  
+  def executeActions() = {
+    //TODO: define actions here
+  }
+}
+
+case class E( master : Orchestrator,  parent : CompositeState) extends State(master, parent) {
+  override def onEntry() = {
+    println("O")
+  }
+  
+  override def onExit() = {
+    //TODO: define actions here
+  }
+}
+
+case class E_Next_L1(previous : State, next : State, master : Orchestrator, var events : List[Event]) extends Transition(previous, next, master, events) { 
+  override def checkGuard : Boolean = {
+    eventsMap.keys.filter{e => e.isInstanceOf[LetterEvent]}.headOption match {
+      case Some(t) => t.asInstanceOf[LetterEvent].letter.equals("L")
+      case None => true
+    }
+  }
+
+  def executeActions() = {
+    //TODO: define actions here
+  }
+}
+
+case class L1( master : Orchestrator,  parent : CompositeState) extends State(master, parent) {
+  override def onEntry() = {
+    println("R")
+  }
+  
+  override def onExit() = {
+    //TODO: define actions here
+  }
+}
+
+case class L1_Next_L2(previous : State, next : State, master : Orchestrator, var events : List[Event]) extends Transition(previous, next, master, events) { 
+  
+  override def checkGuard : Boolean = {
+    eventsMap.keys.filter{e => e.isInstanceOf[LetterEvent]}.headOption match {
+      case Some(t) => t.asInstanceOf[LetterEvent].letter.equals("L")
+      case None => true
+    }
+  }
+  
+  def executeActions() = {
+    //TODO: define actions here
+  }  
+}
+
+case class L2( master : Orchestrator,  parent : CompositeState) extends State(master, parent) {
+  override def onEntry() = {
+    println("L")
+  }
+  
+  override def onExit() = {
+    //TODO: define actions here
+  }
+}
+
+case class L2_Next_O(previous : State, next : State, master : Orchestrator, var events : List[Event]) extends Transition(previous, next, master, events) { 
+  override def checkGuard : Boolean = {
+    eventsMap.keys.filter{e => e.isInstanceOf[LetterEvent]}.headOption match {
+      case Some(t) => t.asInstanceOf[LetterEvent].letter.equals("O")
+      case None => true
+    }
+  }
+
+  def executeActions() = {
+    //TODO: define actions here
+  }
+}
+
+case class O( master : Orchestrator,  parent : CompositeState) extends State(master, parent) {
+  override def onEntry() = {
+    println("D")
+  }
+  
+  override def onExit() = {
+    //TODO: define actions here
+  }
+}
+
+case class O_Next_STOP(previous : State, next : State, master : Orchestrator, var events : List[Event]) extends Transition(previous, next, master, events) { 
+  def executeActions() = {
+    //TODO: define actions here
+  }  
+}
+
+case class STOP( master : Orchestrator,  parent : CompositeState) extends State(master, parent) {
+  override def onEntry() = {
+    println("stop")
+  }
+  
+  override def onExit() = {
+    //TODO: define actions here
+  }
+}
---
+++ src/test/scala/org/sintef/smac/ParemeterTest.scala
@@ -0,0 +1,62 @@
+/**
+ * Licensed under the GNU LESSER GENERAL PUBLIC LICENSE, Version 3, 29 June 2007;
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * 	http://www.gnu.org/licenses/lgpl-3.0.txt
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * Authors: Brice Morin
+ * Company: SINTEF IKT, Oslo, Norway
+ * Date: 2011
+ */
+package org.sintef.smac.test
+
+import org.sintef.smac._
+import org.sintef.smac.samples.parameters._
+
+import org.scalatest.junit.JUnitSuite
+import org.scalatest.junit.ShouldMatchersForJUnit
+import org.junit._
+
+class ParameterTest extends JUnitSuite with ShouldMatchersForJUnit {
+  
+  var master : Orchestrator = _
+  var sm :HelloWorld2StateMachine = _
+  
+  @Before def initialize() {
+    master = new Orchestrator
+    master.start
+    sm = new HelloWorld2StateMachine(master, null, false, false)
+    sm.startState
+  }
+  
+  @Test def verify() {   
+    master ! new LetterEvent("H")
+    Thread.sleep(100)
+    master ! new LetterEvent("E")
+    Thread.sleep(100)
+    master ! new LetterEvent("L")
+    Thread.sleep(100)
+    
+    //LEvent should only trigger one transition
+    //i.e., we should be in L1 state, not in L2 state
+    sm.current should equal (sm.substates.filter{s => s.isInstanceOf[L1]}.head)
+    
+    master ! new LetterEvent("L")
+    Thread.sleep(100)
+    master ! new LetterEvent("O")
+    Thread.sleep(100)
+    
+    //OEvent should trigger on transition from L to O
+    //since stop transition has no associated event, it should also be triggered
+    //so that we should end in the STOP state
+    sm.current should equal (sm.substates.filter{s => s.isInstanceOf[STOP]}.head)
+    
+  }
+}
